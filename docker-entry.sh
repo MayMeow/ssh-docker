ac232e4e0c58d3932bc9f0ea0efbee0038cdead3
Charlotta Jung <jungcharlotta@gmail.com>
2017-02-20 10:14:48 +0100

    Update docker-entry.sh

--- docker-entry.sh
+++ docker-entry.sh
@@ -3,6 +3,8 @@
 echo -ne "Setting user ..."
 
 echo '$SSH_USER:$SSH_PASSWORD' | chpasswd
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
 echo "Starting server..."
 ./usr/sbin/sshd -D
